24caf1fb225b5672dd33da47c3cad2574e5c47f4
Nazarov Yuriy <nazarov.yuriy.pavlovich@gmail.com>
2013-01-27 02:06:19 +0400

    Code cleanup.

--- ladder.js
+++ ladder.js
@@ -1,8 +1,5 @@
 /**
- * Created with JetBrains WebStorm.
- * User: firefish
- * Date: 27/01/13
- * Time: 00:38
+ * Author: Yuriy Nazarov
  */
 
 importPackage(Packages.java.io);
@@ -11,41 +8,46 @@ importPackage(Packages.com.sk89q.worldedit);
 importPackage(Packages.com.sk89q.worldedit.blocks);
 
 var blocks = context.remember();
-var session = context.getSession();
 var player = context.getPlayer();
 
+//get ladder height
 context.checkArgs(1, 1, "<count>");
 
 var count =  parseInt(argv[1]);
 
-context.print(player.getCardinalDirection());
+var direction = ''+player.getCardinalDirection();   //XXX: convert to string
 
 var dx = 1;
 var dy = 1;
 var m = 0;
-if(player.getCardinalDirection() == 'NORTH'){
-    dx = 0;
-    dy = -1;
-    m = 3;
-    context.print("tp1");
-}
-if(player.getCardinalDirection() == 'WEST'){
-    dx = -1;
-    dy = 0;
-    m = 1;
-    context.print("tp2");
-}
-if(player.getCardinalDirection() == 'SOUTH'){
-    dx = 0;
-    dy = 1;
-    m = 2;
-    context.print("tp3");
-}
-if(player.getCardinalDirection() == 'EAST'){
-    dx = 1;
-    dy = 0;
-    m = 0;
-    context.print("tp4");
+
+switch (direction) {
+    case "NORTH":
+    case "NORTH_WEST":
+    case "NORTH_EAST":
+        dx = 0;
+        dy = -1;
+        m = 3;
+        break;
+    case "SOUTH":
+    case "SOUTH_WEST":
+    case "SOUTH_EAST":
+        dx = 0;
+        dy = 1;
+        m = 2;
+        break;
+    case "WEST":
+        dx = -1;
+        dy = 0;
+        m = 1;
+        break;
+    case "EAST":
+        dx = 1;
+        dy = 0;
+        m = 0;
+        break;
+    default:
+        context.error("Something is wrong!");
 }
 
 for (var c = 1; c <= count; c++) {
